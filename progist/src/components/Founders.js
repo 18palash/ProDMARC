3a9e5aeed4ba1ca0238082a6a3c0f18d2ad7785e
Palash Khobragade <18palash@gmail.com>
2019-09-14 15:18:55 +0530

    .

--- progist/src/components/Founders.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const Founders = () => {
-    return(
-        <div>
-            
-        </div>
-    );
-}
---
+++ progist/src/components/about/Founders.js
@@ -0,0 +1,72 @@
+import React from 'react';
+const Founders = () => {
+    return(
+        <div>
+            <div className="section text-center">
+                        <h2 className="title">About Founders</h2>
+                        <div className="team">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="team-player">
+                                        <div className="card card-plain">
+                                            <div className="col-md-6 ml-auto mr-auto">
+                                                <img src="https://static.wixstatic.com/media/2c92e2_198557822c6e4460b85ee0df26a326f4~mv2.jpg/v1/fill/w_308,h_308,al_c,lg_1,q_80/bhavin.webp" alt="Thumbnail Image" className="img-raised rounded-circle img-fluid" />
+                                            </div>
+                                            <h4 className="card-title">Bhavin Bhansali
+                                            <br />
+                                                <small className="card-description text-muted">Founder &amp; CEO</small>
+                                            </h4>
+                                            <div className="card-body">
+                                                <p className="card-description">Bhavin is a cyber security evangelist with over 20 years of combined IT and Information Security 
+                                                work experience. Bhavin has a BFSI work experience of more than 14 years and was a deputy to the CISO in the Information 
+                                                Security Group of ICICI Bank. Bhavin was responsible for all key areas of information and cyber security including governance,
+                                                strategy, incident &amp; vulnerability management and security awareness. Prior to joining ICICI Bank, he was senior consultant 
+                                                at the Business Continuity and Risk Management practice of Wipro Consulting.
+                                                He has played a key role in conceptualising, architecting, implementing and monitoring of various information and cyber 
+                                                security controls including DMARC anti-phishing framework, anti-APT solutions, security analytics and threat intelligence 
+                                                systems, active defense and honey pot systems, data leakage prevention systems, data rights management mechanisms, 
+                                                vulnerability assessment &amp; penetration testing etc.
+                                                Bhavin has also contributed as a speaker in several information and cyber security conferences. He has spoken about 
+                                                several emerging cyber security threats and mitigations. He was also a speaker in the ReBIT (Reserve Bank Information 
+                                                Technology Pvt. Ltd.) organised webinar for all banks on the topic of DMARC.</p>
+                                            </div>
+                                            <div className="card-footer justify-content-center">
+                                                <a href="#pablo" className="btn btn-link btn-just-icon"><i className="fa fa-twitter" /></a>
+                                                <a href="#pablo" className="btn btn-link btn-just-icon"><i className="fa fa-instagram" /></a>
+                                                <a href="https://www.linkedin.com/in/bhavin-bhansali-90b93222/" className="btn btn-link btn-just-icon"><i className="fa fa-linkedin" /></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="team-player">
+                                        <div className="card card-plain">
+                                            <div className="col-md-6 ml-auto mr-auto">
+                                                <img src="https://static.wixstatic.com/media/2c92e2_5c52f3dbdc3e40e0879d8d12da842f40~mv2.png/v1/fill/w_300,h_300,al_c,lg_1,q_80/Chaithanya_v2.webp" alt="Thumbnail Image" className="img-raised rounded-circle img-fluid" />
+                                            </div>
+                                            <h4 className="card-title">Chaithanya Rao
+                                                <br />
+                                                <small className="card-description text-muted">Founder</small>
+                                            </h4>
+                                            <div className="card-body">
+                                                <p className="card-description">Chaithanya is a cyber security veteran with a strong hands-on experience of more than 10 years.
+                                                He was employed with ICICI Bank in the Information Security Group for 8 years. Subsequently, he joined Wipro Technologies 
+                                                and in his stint of 2 years, he has worked on several projects of enterprise security architecture and implementation. He 
+                                                has strong information security hands-on experience in all key tenets of information security including security governance,
+                                                projects, vulnerability and ethical hacking, incident response and security awareness.</p>
+                                            </div>
+                                            <div className="card-footer justify-content-center">
+                                                <a href="#pablo" className="btn btn-link btn-just-icon"><i className="fa fa-twitter" /></a>
+                                                <a href="https://www.linkedin.com/in/chaithanya-rao-cissp-ceh-ab5b8944/" className="btn btn-link btn-just-icon"><i className="fa fa-linkedin" /></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        </div>
+    );
+}
+
+export default Founders;
